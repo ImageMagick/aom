4aab2b75801f42a68ab28db6cb588d69d3dbbcee
Dirk Lemstra <dirk@lemstra.org>
2024-04-29 17:12:02 +0200

    Updated aom to version 3.9.0

--- av1/encoder/block.h
+++ av1/encoder/block.h
@@ -24,7 +24,7 @@
 #include "av1/encoder/enc_enums.h"
 #include "av1/encoder/mcomp_structs.h"
 #if !CONFIG_REALTIME_ONLY
-#include "av1/encoder/partition_cnn.h"
+#include "av1/encoder/partition_cnn_weights.h"
 #endif
 
 #include "av1/encoder/hash_motion.h"
@@ -1348,6 +1348,9 @@ typedef struct macroblock {
   //! Motion vector from superblock MV derived from int_pro_motion() in
   // the variance_partitioning.
   int_mv sb_me_mv;
+  //! Flag to indicate if a fixed partition should be used, only if the
+  // speed feature rt_sf->use_fast_fixed_part is enabled.
+  int sb_force_fixed_part;
   //! SSE of the current predictor.
   unsigned int pred_sse[REF_FRAMES];
   //! Prediction for ML based partition.
